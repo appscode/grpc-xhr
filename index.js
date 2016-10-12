397cfd8e84ff131b535a634f8ab3e6cee7431aed
Tamal Saha <TamalSaha@users.noreply.github.com>
2016-10-12 02:23:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ module.exports = function(path, verb, config, p, body) {
         xhr.onreadystatechange = function() {
             var responseText = this.responseText;
             if (config.skipResponsePrefixLength) {
-                responseText = responseText.substring(config.skipResponsePrefixLength);
+                responseText = responseText.substr(config.skipResponsePrefixLength);
             }
             if (this.readyState === 4) {
                 if (this.status >= 200 && this.status < 400) {
